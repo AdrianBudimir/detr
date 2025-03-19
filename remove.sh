86ff77ef3427c537294b218ee3f2ea63f6ff419b
Adrian Budimir <116813823+AdrianBudimir@users.noreply.github.com>
2025-03-19 16:36:59 +0100

    Create remove.sh

---
+++ remove.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+
+# Deregister / Uninstall FortiClient, Uninstall Bitdefender, remove /opt/trimblesw, Uninstall CrashPlan, Delete certs, change helpdesk_local password, and log actions
+
+LOG_FILE2="/var/log/maverickrem.log"
+TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
+
+logme() {
+  echo "$TIMESTAMP: $1" >> "$LOG_FILE2"
+  echo "$TIMESTAMP: $1" # Also print to console
+}
+
+# Deregister FortiClient
+if command -v /opt/forticlient/epctrl &> /dev/null; then
+  sudo /opt/forticlient/epctrl -u
+  logme "FortiClient deregistered."
+
+  # Purge FortiClient
+  if command -v apt-get &> /dev/null; then
+    sudo apt-get purge -y forticlient*
+    if [ $? -eq 0 ]; then
+      logme "FortiClient purged."
+      # Delete FortiClient repo file
+      for file in /etc/apt/sources.list.d/repo.forticlien*; do
+        if [ -f "$file" ]; then
+          sudo rm "$file"
+          if [ $? -eq 0 ]; then
+            logme "FortiClient repo file $file deleted."
+          else
+            logme "Failed to delete FortiClient repo file $file."
+          fi
+        else
+          logme "FortiClient repo file $file not found."
+        fi
+      done
+
+    else
+      logme "Failed to purge FortiClient."
+    fi
+  else
+    logme "apt-get not found. Skipping purge."
+  fi
+
+else
+  logme "FortiClient not found. Skipping deregistration and purge."
+fi
+
+# Remove Bitdefender GravityZone
+if [ -f "/opt/bitdefender-security-tools/bin/uninstall" ]; then
+  sudo /opt/bitdefender-security-tools/bin/uninstall
+  if [ $? -eq 0 ]; then
+    logme "Bitdefender GravityZone uninstalled via script."
+  else
+    logme "Uninstall Failed or does not exist, Bitdefender GravityZone via script."
+  fi
+elif command -v apt-get &> /dev/null; then
+  sudo apt-get purge -y bitdefender-security-tools
+  if [ $? -eq 0 ]; then
+    logme "Bitdefender GravityZone purged via apt."
+  else
+    logme "Failed to purge Bitdefender GravityZone via apt."
+  fi
+else
+  logme "Bitdefender GravityZone uninstall script and apt-get not found. Skipping Bitdefender removal."
+fi
+
+# Remove /opt/trimblesw
+if [ -d "/opt/trimblesw" ]; then
+  sudo rm -rf /opt/trimblesw
+  if [ $? -eq 0 ]; then
+    logme "/opt/trimblesw removed."
+  else
+    logme "Failed to remove /opt/trimblesw."
+  fi
+else
+  logme "/opt/trimblesw directory not found. Skipping removal."
+fi
+
+# Delete Certificates
+CERTIFICATES="/etc/ssl/certs/Trimble-CA2.pem /etc/ssl/certs/Trimble-CA3.pem /etc/ssl/certs/Trimble-CA.pem /etc/ssl/certs/Trimble-CAs.pem /etc/ssl/certs/Trimble-SCCM-CA.pem"
+
+for cert in $CERTIFICATES; do
+    if [ -f "$cert" ]; then
+        sudo rm "$cert"
+        if [ $? -eq 0 ]; then
+            logme "Certificate $cert deleted."
+        else
+            logme "Failed to delete certificate $cert."
+        fi
+    else
+        logme "Certificate $cert not found. Skipping deletion."
+    fi
+done
+
+# Removing Crashplan
+if [ -d "/usr/local/crashplan" ]; then
+  logme "CrashPlan found in /usr/local/crashplan. Running uninstaller."
+  sudo ./uninstallcp.sh -i /usr/local/crashplan -y
+  if [ $? -eq 0 ]; then
+    logme "CrashPlan uninstalled successfully."
+  else
+    logme "CrashPlan uninstallation failed."
+  fi
+else
+  logme "CrashPlan not found in /usr/local/crashplan. Skipping uninstallation."
+fi
+
+# Change helpdesk_local password
+if getent passwd helpdesk_local &> /dev/null; then
+  logme "Changing helpdesk_local password..."
+  sudo bash -c "echo 'helpdesk_local:byebyetrimble' | chpasswd"
+  if [ $? -eq 0 ]; then
+    logme "helpdesk_local password changed successfully."
+  else
+    logme "Failed to change helpdesk_local password."
+  fi
+
+else
+  logme "helpdesk_local user not found. Skipping password change."
+fi
+
+exit 0
